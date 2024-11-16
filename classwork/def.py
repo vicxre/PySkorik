ecdb5ca3364f751c9cb1c87055e8207aa5ef09bc
Olezaaa <kirskor1337@gmail.com>
2024-11-16 16:19:32 +0300

    доделал пз4

---
+++ classwork/def.py
@@ -0,0 +1,64 @@
+# def my_def()
+#
+#
+#
+#
+#
+#
+#
+#
+
+#
+# def my_sum(a, b):
+#     if a > b:
+#         return a + b
+#     else:
+#         return a - b
+#
+#
+# num1 = int(input("Введи 1 число"))
+# num2 = int(input("Введи 2 число"))
+# print("результат = ", my_sum(num1, num2))
+
+
+# def my_sum(a, b):
+#    return a+ b, a * b
+#
+#
+# num1 = int(input("Введи 1 число"))
+# num2 = int(input("Введи 2 число"))
+#
+# s,d = my_sum(num1, num2)
+#
+# print("результат cуммы  = ", s )
+# print("результат произведения  = ", d )
+
+
+
+# var1 = 'Global'
+#
+#
+# def foo():
+#     global var2, var1
+#     var1 = 'local'
+#     var2 = 'local2'
+#     print(var1)
+#
+#
+# print(var1)
+# foo()
+# print(var2)
+
+def my_def(*args):
+    print(args)
+    return 'true'
+
+
+my_def( 1, 2, "true", 2.36)
+print(my_def())
+
+
+
+
+
+
