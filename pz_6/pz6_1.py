f3ee6a30622f180ee5d69ca42dd13191adbc3be1
kirr <kirskor1337@gmail.com>
2024-12-18 00:46:14 +0300

    делаю

--- pz_6/pz6_1.py
+++ pz_6/pz6_1.py
@@ -2,3 +2,18 @@
 #нечетные числа. Если чередуются, то вывести 0, если нет, то вывести порядковый
 #номер первого элемента, нарушающего закономерность.
 
+
+# random для пяти чисел
+a1, a2, a3, a4, a5 = (random.randint(1, 99), random.randint(1, 99),
+                      random.randint(1, 99), random.randint(1, 99),
+                      random.randint(1, 99))
+
+# до пропуска через саму функцию
+print(a1, a2, a3, a4, a5)
+
+a1 = DigitCountSum(a1)
+a2 = DigitCountSum(a2)
+a3 = DigitCountSum(a3)
+a4 = DigitCountSum(a4)
+a5 = DigitCountSum(a5)
+
