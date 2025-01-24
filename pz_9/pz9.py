2e83e709417ecf55bb9edaaf669faa68f1f9e138
kirr <kirskor1337@gmail.com>
2025-01-25 02:17:07 +0300

    делаю

--- pz_9/pz9.py
+++ pz_9/pz9.py
@@ -3,28 +3,41 @@
 #строки в словари, с использованием функции найти среднее значение продаж по
 #каждому виду продукции, результаты вывести на экран.
 
-orange = {}
-inf = "45 991 63 100 12"
-inf = inf.split()
-orange['MONDAY'] = inf[0]
-orange['TUESDAY'] = inf[1]
-orange['WEDNESDAY'] = inf[2]
-orange['THURSDAY'] = inf[3]
-orange['FRIDAY'] = inf[4]
+def averageO(sum_orange,long_orange):
+
+    orange = {}
+    int.inf = "45 991 63 100 12"
+    inf = int.inf.split()
+    orange['MONDAY'] = inf[0]
+    orange['TUESDAY'] = inf[1]
+    orange['WEDNESDAY'] = inf[2]
+    orange['THURSDAY'] = inf[3]
+    orange['FRIDAY'] = inf[4]
+
+    sum_orange = sum(inf)
+    long_orange = len(inf)
+    average_orange = sum_orange / long_orange
+    return average_orange
+
+averageO(sum_orange,long_orange)
+resultO = average_orange
+print(resultO)
+
+
 
 apple = {}
-inf = "13 47 26 0 16"
-inf = inf.split()
+int.inf = "13 47 26 0 16"
+inf = int.inf.split()
 apple['MONDAY'] = inf[0]
 apple['TUESDAY'] = inf[1]
 apple['WEDNESDAY'] = inf[2]
 apple['THURSDAY'] = inf[3]
 apple['FRIDAY'] = inf[4]
-print('Апельсины - ',orange,'Яблоки',apple)
+print('Яблоки продажи с пнд до пт - ',apple)
 
 
 
 
 
-#average в самой функции сделать срезы сложить и делить на количество
+
 
