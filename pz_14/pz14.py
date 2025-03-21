679e2f8e25df78de766fb06f45aae25e5f841f23
kir <kirskor1337@gmail.com>
2025-03-22 01:00:58 +0300

    el psy congroo

---
+++ pz_14/pz14.py
@@ -0,0 +1,8 @@
+#В исходном текстовом файле(Dostoevsky.txt) найти все произведения писателя.
+#Посчитать количество полученных элементов.
+
+f1 = open('Dostoevsky.txt', 'r', encoding='utf-8')
+
+txt =f1.read()
+
+composition =
