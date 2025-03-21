d5dc36f3a1fa166ef002f7a2f238eb6b3e661071
kir <kirskor1337@gmail.com>
2025-03-22 00:28:55 +0300

    el psy congroo

--- pz_13/pz13_2.py
+++ pz_13/pz13_2.py
@@ -1,1 +1,17 @@
 #В двумерном списке элементы второго столбца возвести в квадрат.
+
+spisok = [[1,2,3,4,5],[6,7,8,9,10]]
+
+second = spisok[1]
+squared = [x ** 2 for x in second]
+
+print(squared)
+
+
+
+
+
+#quadro = map(second ** 2)
+#pow
+
+
