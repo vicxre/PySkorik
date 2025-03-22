06f595b729850ab7b54b643d5a09df58396632cb
kir <kirskor1337@gmail.com>
2025-03-23 02:31:02 +0300

    el psy congroo

--- pz_9/pz9.py
+++ pz_9/pz9.py
@@ -2,72 +2,33 @@
 #отражающая продажи продукции по дням в кг. Преобразовать информацию из
 #строки в словари, с использованием функции найти среднее значение продаж по
 #каждому виду продукции, результаты вывести на экран.
-def averageO(sum_orange,long_orange):
 
-    orange = {}
-    int.n = "45 991 63 100 12"
-    n = int.split()
-    orange['MONDAY'] = n[0]
-    orange['TUESDAY'] = n[1]
-    orange['WEDNESDAY'] = n[2]
-    orange['THURSDAY'] = n[3]
-    orange['FRIDAY'] = n[4]
+orange = '45 991 63 100 12'
+oranges = [int(x) for x in orange.split(' ')]
 
-    sum_orange = sum(n)
-    long_orange = len(n)
-    average_orange = sum_orange / long_orange
-    return average_orange
+def averageo(sumo,longo):
+    aver = sumo / longo
+    return aver
+sumo = sum(oranges)
+longo = len(oranges)
+resulto = averageo(sumo,longo)
 
-print(average0)
-print(orange)
+#vtoroe
+apple = "13 47 26 0 16"
+apples = [int(z) for z in apple.split(' ')]
 
+def averagea(suma,longa):
+    avera = suma / longa
+    return avera
+suma = sum(apples)
+longa = len(apples)
+resulta = averagea(sumo,longo)
 
-apple = {}
-int.inf = "13 47 26 0 16"
-inf = int.inf.split()
-apple['MONDAY'] = inf[0]
-apple['TUESDAY'] = inf[1]
-apple['WEDNESDAY'] = inf[2]
-apple['THURSDAY'] = inf[3]
-apple['FRIDAY'] = inf[4]
-print('Яблоки продажи с пнд до пт - ',apple)
+#в словарь
+o = {'пнд':oranges[0],'вт':oranges[1],'ср':oranges[2],'чт':oranges[3],'пт':oranges[4]}
+a = {'пнд':apples[0],'вт':apples[1],'ср':apples[2],'чт':apples[3],'пт':apples[4]}
 
-
-
-
-#def averageO():
-    # Данные о количестве апельсинов каждый день недели
- #   n = "45 991 63 100 12"
-
-    # Преобразуем строку в список целых чисел
-  #  numbers = list(map(int, n.split()))
-
-    # Создадим словарь с днями недели и количеством апельсинов
-   # orange = {
-    #    'MONDAY': numbers[0],
-     #   'TUESDAY': numbers[1],
-      #  'WEDNESDAY': numbers[2],
-       # 'THURSDAY': numbers[3],
-        #'FRIDAY': numbers[4]
-    #}
-
-    # Суммируем количество апельсинов за всю неделю
-#   sum_orange = sum(numbers)
-
-    # Определяем длину списка (количество дней)
-    #long_orange = len(numbers)
-
-    # Вычисляем среднее количество апельсинов в день
-    #average_orange = sum_orange / long_orange
-
-
-    #return average_orange
-    #return orange
-
-
-# Вызов функции и вывод результата
-#resultO = averageO()
-#print(resultO)
-#print(orange)
-
-
+print('продажи по дням апельсины: ',o)
+print('среднее значение продаж апельсинов: ',resulto)
+print('продажи по дням яблоки: ',a)
+print('среднее значение продаж яблок: ',resulta)
