405d1cf09db02f84586a7a221e34d2fbb2793471
kir <kirskor1337@gmail.com>
2025-03-23 19:31:06 +0300

    el psy congroo

--- pz_12/pz12_2.py
+++ pz_12/pz12_2.py
@@ -1,1 +1,18 @@
 #Составить генератор (yield), который выводит из строки только цифры.
+
+from unicodedata import digit
+
+num = '13vc27holymoly'
+
+def numbers(num):
+
+
+    for n in num:
+        if num.isdigit():
+            yield
+
+
+
+#yield from [x for x in num.isdigit()]
+#print(numbers)
+
