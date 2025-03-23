198b8fa1849cc61425985cbe6ab6d2a58f82b342
kir <kirskor1337@gmail.com>
2025-03-23 21:35:00 +0300

    el psy congroo

--- pz_7/pz7_2.py
+++ pz_7/pz7_2.py
@@ -1,15 +1,30 @@
 #Даны строки S, S1 и S2. Заменить в строке S последнее вхождение строки S1 на строку
 #S2.
+from re import search
 
-s = 'yes no'
-s1 = 'да'
-s2 = 'нет'
+s = 'im да'
+s1 = 'no'
+s2 = 'yes'
 
-b = s1[::-1]
 
-a = s1.find('тен']
+#search = rfind()
+zamena = s.replace(s,s1)
 
-print(a,b)
+print(s)
 
+
+
+
+
+
+
+
+
+
+
+#b = s1[::-1]
+#a = s1.find('тен')
 #rfind(
 #replace(s)
+#добавить к s да и заменить на нет
+#добвить ад и реплейс первый индекс
