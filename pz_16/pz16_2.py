48bfa200e080a7fa5b3e9b9ec060f1c903c0be6a
kir <kirskor1337@gmail.com>
2025-06-10 01:58:50 +0300

    v potoke

--- pz_16/pz16_2.py
+++ pz_16/pz16_2.py
@@ -7,26 +7,27 @@
 #bibika
 class car:
 
-    def __init__(self, brand, model, years, gruz, kolvo):
+    def __init__(self, brand, model, years):
         self.brand = brand
         self.model = model
         self.years = years
 
-    def inform(self):
+    def car(self):
 
-
+        return f'brand:{self.brand},model:{self.model},year of release:{self.years}'
 
 
 #gruzovik
 class truck(car):
 
-    def infa(self, gruz):
-        self.gruz = 0
+    def __init__(self, brand, model, years, gruzo):
+        super().__init__(brand, model, years)
+        self.gruzo = gruzo
 
 
 
 
 
 
-tachila = car('bmw', 'm5 f90', '2023')
+tachila = car(' bmw ', ' m5f90 ', ' 2023. ')
 massa = truck('8тонн')
