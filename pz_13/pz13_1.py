b30da5da96e305073e661882af8d0f08baebbdb7
kir <kirskor1337@gmail.com>
2025-06-21 01:40:11 +0300

    ✌️✌️

--- pz_13/pz13_1.py
+++ pz_13/pz13_1.py
@@ -1,14 +1,14 @@
 #В двумерном списке найти сумму элементов второй половины матрицы.
 
-import random
+import random as r
 
 
-matric = [[random.randint(1, 10) for x in range(1, 5)] for y in range(random.randint(3, 4))]
-print("spisok: ", matric)
-answ = []
-
-for i in matric:
-    answ += i[1:3][i]
 
 
-print(sum(str(answ[1])))
+matrix = [[r.randint(1, 10) for x in range(1, 5)] for y in range(r.randint(3, 4))]
+
+print(matrix)
+
+
+#print(sum(str(answ[1])))
+
