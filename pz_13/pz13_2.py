feac28747732febe82336165322f5604283292c9
kir <kirskor1337@gmail.com>
2025-06-23 22:01:47 +0300

    сделал 13 2

--- pz_13/pz13_2.py
+++ pz_13/pz13_2.py
@@ -1,28 +1,17 @@
 #В двумерном списке элементы второго столбца возвести в квадрат.
 
-
-import random
-
-
-matric = [[random.randint(1, 10) for x in range(1, 4)] for y in range(random.randint(3, 5))]
-print("spisok: ", matric)
-answ = []
-
-for i in matric:
-    answ += i[1:3][i]
+import random as r
 
 
 
-second = spisok[1]
-squared = [x ** 2 for x in second]
-
-print(squared)
+matrix = [[r.randint(1, 10) for _ in range(3)] for _ in range(3)]
+print("исходная: ", matrix)
 
 
+for new in matrix:
+    new[1] *= new[1]
 
-
-
-#map
-#pow
-
-
+#перебор циклом для вывода всех трех списков
+print('матрица после возведения в квадрат:')
+for new in matrix:
+    print(new)
