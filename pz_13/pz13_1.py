e1ca97e0e3bec82b05244b47d42126b80b56f913
kir <kirskor1337@gmail.com>
2025-06-24 13:25:36 +0300

    сделал 13 1

--- pz_13/pz13_1.py
+++ pz_13/pz13_1.py
@@ -4,11 +4,12 @@ import random as r
 
 
 
-
 matrix = [[r.randint(1, 10) for _ in range(4)] for _ in range(4)]
-#вторая половина
 
+
+#вторая половина
 polovina = len(matrix) // 2
 
-sum_matr = sum(sum(row) for row in matrix[polovina:])
+sum_matr = sum(sum(non) for non in matrix[polovina:])
+
 print('исходаня матрица:',matrix, 'сложение второй половины: ',sum_matr)
