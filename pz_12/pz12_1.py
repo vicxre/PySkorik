865398786d61beda75565cf38f6a3ce9538753fc
kir <kirskor1337@gmail.com>
2025-06-25 02:33:33 +0300

    сделал 13 1

--- pz_12/pz12_1.py
+++ pz_12/pz12_1.py
@@ -1,14 +1,51 @@
 # Дана последовательность целых чисел. Поменять местами ее первую и
 # последнюю трети.
+
+
+num = list(range(1, 13))
+
+
+first = 0
+
+last = len(num) - len(num)//3
+
+new_num = (last + first)
+
+print(new_num)
+
+
+
+
+
+
+#tret = len(num) // 3
+
+#zamena = tret.replace[[0],[2]]
+
+#print(zamena)
+
+#for zamena in num:
+ #   print(zamena)
+
+#zamena = tret.replace(num)
+
+
 #from functools import reduce
 
 
-#num = [1,2,3,4,5,6]
+#zamena = tret.replace([0],[2])
+
+#print(zamena)
+
+
+
+
+
+
 #tret = [num in num//3 for num]
 
-print(list(filter(lambda x: x % 2 == [1, 2, 4, 5, 6])))
 
-
+#print(list(filter(lambda x: x % 2 == [1, 2, 4, 5, 6])))
 
 
 #print(reduce(lambda x,y: y-x))
