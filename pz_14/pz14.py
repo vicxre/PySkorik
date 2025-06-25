134025fb450fa43fc877097feec95a957158bff7
kir <kirskor1337@gmail.com>
2025-06-26 01:20:12 +0300

    делаю пз 14

--- pz_14/pz14.py
+++ pz_14/pz14.py
@@ -5,7 +5,25 @@ import re
 
 
 f1 = open('Dostoevsky.txt', 'r', encoding='utf-8')
+txt = f1.read()
 
-txt =f1.read()
+pattern = r'(?i)(?:роман|повесть|рассказ|опубл\.?)([^a-zA-Z]*([А-ЯЁ][\w\s]+))'
+works = re.findall(pattern, txt)
+unique_titles = set(works)
 
-#composition
+
+count_unique_titles = len(unique_titles)
+
+for title in unique_titles:
+    print(title)
+
+print('кол-во произведений:', count_unique_titles)
+
+
+
+
+
+
+
+
+
