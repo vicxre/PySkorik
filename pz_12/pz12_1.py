2febb751bae3c90a96fed5b76706a9b4db5c0623
kir <kirskor1337@gmail.com>
2025-06-25 23:14:32 +0300

    made 12 pz

--- pz_12/pz12_1.py
+++ pz_12/pz12_1.py
@@ -1,22 +1,16 @@
 # Дана последовательность целых чисел. Поменять местами ее первую и
 # последнюю трети.
 
+num = list(range(1, 10))
+
 def zamena(num):
 
-    num = list(range(1, 13))
-
     parts = len(num) // 3
+    return (lambda seq, t: seq[2*t:] + seq[t:2*t] + seq[:t])(num, parts)
 
+result = zamena(num)
+print('до: ', num,'после: ',result)
 
-
-first = num[:parts]
-mid = num[parts:-parts]
-last = num[-parts:]
-
-new_num = last + mid + first
-
-print('до: ',num)
-print('после: ',new_num)
 
 
 
@@ -44,7 +38,6 @@ print('после: ',new_num)
 #zamena = tret.replace([0],[2])
 
 #print(zamena)
-
 
 
 #tret = [num in num//3 for num]
