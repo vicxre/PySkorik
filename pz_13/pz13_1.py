bcabf4ada519c8482043073bc560b40bad42dba7
kir <kirskor1337@gmail.com>
2025-06-26 18:59:12 +0300

    подправил пз 13

--- pz_13/pz13_1.py
+++ pz_13/pz13_1.py
@@ -1,15 +1,13 @@
-#В двумерном списке найти сумму элементов второй половины матрицы.
+#В двумерном списке найти сумму элементов второй половины матрицы
 
 import random as r
 
-
-
-matrix = [[r.randint(1, 10) for _ in range(4)] for _ in range(4)]
-
+visota = int(input('высота:'))
+shirina = int(input('ширина:'))
+matrix = [[r.randint(1, 10) for _ in range(shirina)] for _ in range(visota)]
 
 #вторая половина
 polovina = len(matrix) // 2
-
 sum_matr = sum(sum(non) for non in matrix[polovina:])
 
-print('исходная матрица:',matrix, 'сложение второй половины: ',sum_matr)
+print('исходная матрица:', matrix, 'сложение второй половины: ', sum_matr)
--- pz_13/pz13_2.py
+++ pz_13/pz13_2.py
@@ -2,15 +2,14 @@
 
 import random as r
 
-
-matrix = [[r.randint(1, 10) for _ in range(3)] for _ in range(3)]
+visota = int(input('высота:'))
+shirina = int(input('ширина:'))
+matrix = [[r.randint(1, 10) for _ in range(shirina)] for _ in range(visota)]
 print("исходная: ", matrix)
-
 
 for new in matrix:
     new[1] *= new[1]
 
-#перебор циклом для вывода всех трех списков
 print('матрица после возведения в квадрат:')
 for new in matrix:
     print(new)
