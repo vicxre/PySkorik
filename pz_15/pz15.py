64c9746bdfe76d1703504a8682d329097ff4b644
kir <kirskor1337@gmail.com>
2025-06-26 16:51:10 +0300

    делаю пз15

--- pz_15/pz15.py
+++ pz_15/pz15.py
@@ -4,4 +4,29 @@
 
 import sqlite3 as sq
 
-with sq.connect("baza_hospital.db") as con:
+# Создание базы данных и таблицы Пациент
+with sq.connect("polyclinic.db") as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS patients (
+    patient_id INTEGER PRIMARY KEY NOT NULL,
+    patient_name VARCHAR(50) NOT NULL,
+    doctor_name VARCHAR(50) NOT NULL,
+    diagnosis TEXT,
+    treatment_cost DECIMAL(10, 2) NOT NULL
+    )""")
+
+#вывести все строчки, где стоимость лечения больше 5000
+with sq.connect("polyclinic.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM patients WHERE treatment_cost > 5000")
+    result = cur.fetchall()
+    print("Пациенты с стоимостью лечения больше 5000:", result)
+
+#вывести информацию о пациентах конкретного врача
+with sq.connect("polyclinic.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM patients WHERE doctor_name = 'Иванова'")
+    result1 = cur.fetchone()
+    result2 = cur.fetchmany(2)
+    print("Первый пациент врача Иванова:", result1)
+    print("Следующие два пациента врача Иванова:", result2)
